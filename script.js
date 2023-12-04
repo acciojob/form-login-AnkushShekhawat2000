f7194d39321d1d6f5529a5717c420e813ef50e8a
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-04 23:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function getFormvalue() {
-    //Write your code here
-	let x = document.getElementById("form1");
-	let name = "";
-	for(let i =0; i<x.length; i++){
-		if(x.elements[i].value!="Submit"){
-			name += x.elements[i]+" "
-		}
-		
-	}
-alert(name.substring(0,name.lenth-1));
+  // Write your code here
+  let x = document.getElementById("form1");
+  let name = "";
+
+  for (let i = 0; i < x.length; i++) {
+    if (x.elements[i].type !== "submit") {
+      name += x.elements[i].value + " ";
+    }
+  }
+
+  alert(name.trim());
 }
